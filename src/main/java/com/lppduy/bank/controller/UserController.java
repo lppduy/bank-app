10aaea3373c90068bb34aaf69bb3a3e0971048d9
lppduy <lephamphuongduy@gmail.com>
2024-02-18 15:23:43 +0700

    Added credit and debit service

--- src/main/java/com/lppduy/bank/controller/UserController.java
+++ src/main/java/com/lppduy/bank/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.lppduy.bank.controller;
 
 import com.lppduy.bank.dto.BankResponse;
+import com.lppduy.bank.dto.CreditDebitRequest;
+import com.lppduy.bank.dto.EnquiryRequest;
 import com.lppduy.bank.dto.UserRequest;
 import com.lppduy.bank.service.EmailService;
 import com.lppduy.bank.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
@@ -25,4 +24,22 @@ public class UserController {
         return userService.createAccount(userRequest);
     }
 
+    @GetMapping("/balanceEnquiry")
+    public BankResponse balanceEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
+        return userService.balanceEnquiry(enquiryRequest);
+    }
+    @GetMapping("/nameEnquiry")
+    public String nameEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
+        return userService.nameEnquiry(enquiryRequest);
+    }
+
+    @PostMapping("/credit")
+    public BankResponse creditAccount(@RequestBody CreditDebitRequest request) {
+        return userService.creditAccount(request);
+    }
+
+    @PostMapping("/debit")
+    public BankResponse debitAccount(@RequestBody CreditDebitRequest request) {
+        return userService.debitAccount(request);
+    }
 }
